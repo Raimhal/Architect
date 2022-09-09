e4f0d4f6805ed7e0923d1249a8a43a3b997c4cbd
Roman Kormysh <71661562+Raimhal@users.noreply.github.com>
2022-09-09 17:27:42 +0300

    Bug/production images bug (#132)
    * fix project images
    
    * images bug fixing
    
    * last gix
    
    * .

--- frontend/src/app/modules/project/resources/pipes/imagePipe.ts
+++ frontend/src/app/modules/project/resources/pipes/imagePipe.ts
@@ -7,7 +7,7 @@ export class ImagePipe implements PipeTransform {
   transform(input: IProjectPhoto[]): IImage[] {
     return input.map((f) => {
       return {
-        path: environment.production ? f.link : `${environment.filesBaseUrl}/${f.link}`,
+        path: f.link.includes("http") ? f.link : `${environment.filesBaseUrl}/${f.link}`
       };
     });
   }
